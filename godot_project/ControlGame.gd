43d8e5e1c14fb69c393b95a985a2625f61e01fcc
ArthurZucker <arthur.zucker@gmail.com>
2020-06-01 20:12:45 +0200

    merging home

--- godot_project/ControlGame.gd
+++ godot_project/ControlGame.gd
@@ -62,23 +62,4 @@ func createTile(x,y,tilenum):
 	$Spatial.add_child(mi)
 	return mi
 	
-func createExplorer(x,y,num):
-	# Create a new tile instance
-	var mi=MeshInstance.new()
-	# and translate it to its final position
-	mi.set_translation(Vector3(x,0,y))
-	mi.set_rotation(Vector3(0,PI/2.0,0))
-	mi.set_scale(Vector3(0.5,0.5,0.5))
-	# load the tile mesh
-	var meshObj=load("res://final_card005.obj")
-	# and assign the mesh instance with it
-	mi.mesh=meshObj
-	# create a new spatial material for the tile
-	var surface_material=SpatialMaterial.new()
-	# set its color
-	#surface_material.albedo_color=explorerColor[num]
-	# and assign the material to the mesh instance
-	mi.set_surface_material(0,surface_material)
-	# add the newly created instance as a child of the Origine3D Node
-	$Spatial.add_child(mi)
-	return mi
+
